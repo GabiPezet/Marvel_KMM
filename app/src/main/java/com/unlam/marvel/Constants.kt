d50a02400176bfd2cada5a9f8785c18e59771b57
Gabriel Pezet <gabriel.pzt@gmail.com>
2024-09-14 16:01:10 -0300

    API Key public and private added

--- app/src/main/java/com/unlam/marvel/Constants.kt
+++ app/src/main/java/com/unlam/marvel/Constants.kt
@@ -1,4 +1,4 @@
 package com.unlam.marvel
 
-internal val PUBLIC_KEY = "your_public_key"
-internal val PRIVATE_KEY = "your_private_key"
+internal val PUBLIC_KEY = "f770bf86c250f5cef375f07f7f34e275"
+internal val PRIVATE_KEY = "7f16b02062e2709d8ccc84b84b7cc87a4d80bef5"
